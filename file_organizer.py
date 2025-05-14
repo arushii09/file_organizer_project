bc8c65a977ea70912ebcc53e2f5095d0d1e4c5c2
arushii09 <arushiii323@gmail.com>
2025-05-15 01:14:45 +0530

    Update file_organizer.py

--- file_organizer.py
+++ file_organizer.py
@@ -9,23 +9,23 @@ zips = [".zip", ".rar"]
 software = [".exe", ".apk"]
 
 path = "C:\\Users\\Arushi\\OneDrive\\Desktop\\docs"
-files = os.listdir(path)  #to get a list of all files and subdirectories in the specified path
+files = os.listdir(path) 
 
-for folder in ["audio", "video", "documents", "images", "zips", "software", "unknown"]:  # Create folders if they don't exist
+for folder in ["audio", "video", "documents", "images", "zips", "software", "unknown"]: 
     folder_path = os.path.join(path, folder)  
-    if not os.path.exists(folder_path):  # checks if the folder already exists using os.path.exists()
-        os.mkdir(folder_path)  #If the folder does not exist, it creates the folder using os.mkdir()
+    if not os.path.exists(folder_path): 
+        os.mkdir(folder_path) 
 
 # Move files
 for file in files:
-    extension = os.path.splitext(file)[1]  #Extracts the file extension of each file by splitting the file name from its extension
-    full_file_path = os.path.join(path, file)  #Constructs the complete path for the file 
+    extension = os.path.splitext(file)[1]  
+    full_file_path = os.path.join(path, file)  
 
-    if extension == "":                                  #If the file doesn’t have an extension the code moves on to check if it's a subfolder
-        subfolder_path = os.path.join(path, file)   #checks if file is a folder then it lists all files inside that subfolder
+    if extension == "":                                
+        subfolder_path = os.path.join(path, file)  
         if os.path.isdir(subfolder_path):  
             for file1 in os.listdir(subfolder_path):
-                ext = os.path.splitext(file1)[1]     #each file inside the subfolder(file1), it checks the extension and determines           which folder to move it to (e.g., audio, video  etc.)
+                ext = os.path.splitext(file1)[1]    
                 source = os.path.join(subfolder_path, file1)
 
                 if ext in audio:
@@ -44,8 +44,7 @@ for file in files:
                     dest = os.path.join(path, "unknown", file1)
 
                 if not os.path.exists(dest):
-                    shutil.move(source, dest)  # moves the file to the appropriate destination folder, but only if it doesn’t already exist there
-
+                    shutil.move(source, dest) 
     else:
         if extension in audio:
             dest_folder = "audio"
